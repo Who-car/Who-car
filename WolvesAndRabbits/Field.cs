ef5950e6d0a88a4b7f3487145a7ca3f0759221e6
Aysulu Gataullina <aysulu2645@gmail.com>
2023-05-20 12:16:31 +0300

    1/3 stage of work. Parallel task fixed

--- WolvesAndRabbits/Field.cs
+++ WolvesAndRabbits/Field.cs
@@ -1,89 +1,124 @@
-﻿namespace WolvesAndRabbits;
+﻿using Treaty;
+
+namespace WolvesAndRabbits;
 
 using System.Threading.Tasks;
-public class Field
+public class Field : IMapManager
 {
-    protected int[,] _field; 
+    private ReaderWriterLockSlim _lockSlim;
+    private int[,] _field;
+    private bool _isEmpty;
+    private int N;
+    private int M;
     
     public Field(int n, int m)
     {
-        _field = new int[n+2, m+2];
-        var rnd = new Random();
-        for (var i = 1; i < n+1; i++)
-        for (var j = 1; j < m + 1; j++)
-            _field[i, j] = rnd.Next(3);
+        _lockSlim = new ReaderWriterLockSlim();
+        _field = new int[n, m];
+        _isEmpty = true;
+        N = n;
+        M = m;
     }
 
-    public void ChangeFieldState()
+    public void Clear()
     {
-        var field = _field;
-        var tasks = new Task[field.GetLength(1)];
-        for (var i = 1; i < field.GetLength(1)-1; i++)
+        for (var i = 0; i < N; i++)
+        for (var j = 0; j < M; j++)
+            _field[i, j] = 0;
+        _isEmpty = true;
+    }
+
+    public void UpdateMap()
+    {
+        if (_isEmpty)
+            GenerateMap();
+        var tasks = new Task[M];
+        for (var i = 0; i < M; i++)
         {
             var i1 = i;
-            tasks[i1] = Task.Run(() => GoThroughColumn(i1, field));
-            tasks[i1].Wait();
+            tasks[i1] = Task.Run(() => GoThroughColumn(i1, _field));
         }
+        Task.WaitAll(tasks);
     }
     
-    private static void GoThroughColumn(int col, int[,] field)
+    private void GoThroughColumn(int col, int[,] field)
     {
-        for (var i = 1; i < field.GetLength(0)-1; i++)
+        for (var i = 0; i < N; i++)
         {
             var neighbours = CheckAliveNeighboursCount(i, col, field);
-            if (field[i, col] == 0)
+            switch (field[i, col])
             {
-                field[i, col] = neighbours == 3 ? 1 : 0;
-            }
-            else if (field[i, col] == 1)
-            {
-                field[i, col] = neighbours < 2 ? 0
-                    : neighbours < 5 ? 1
-                    : 0;
-            }
-            else
-            {
-                field[i, col] = neighbours < 2 ? 0
-                    : neighbours < 5 ? 1
-                    : 0;
-                TryEatRabbit(i, col, field);
+                case 2:
+                    field[i, col] = neighbours < 2 ? 0
+                        : neighbours < 4 ? 2
+                        : 0;
+                    if (neighbours is >= 2 and < 4)
+                        TryEatRabbit(i, col, field);
+                    break;
+                case 1:
+                    field[i, col] = neighbours < 2 ? 0
+                        : neighbours < 5 ? 1
+                        : 0;
+                    break;
+                default:
+                    field[i, col] = neighbours < 2 ? 0
+                        : neighbours < 4 ? new Random().Next(1, 3)
+                        : 0;
+                    break;
             }
         }
     }
     
-    private static int CheckAliveNeighboursCount(int x, int y, int[,] field)
+    private int CheckAliveNeighboursCount(int x, int y, int[,] field)
     {
         var count = 0;
         for (var i = x-1; i <= x+1; i++)
         for (var j = y - 1; j <= y + 1; j++)
-            count += i == x && j == y ? 0
-                : field[i, j] == 0 ? 0
-                : 1;
+        {
+            if (i < 0 || i >= N || j < 0 || j >= M) continue;
+            _lockSlim.EnterReadLock();
+            try
+            {
+                count += field[i, j] == 0 ? 0 : 1;
+            }
+            finally
+            {
+                _lockSlim.ExitReadLock();
+            }
+        }
         return count;
     }
 
-    private static void TryEatRabbit(int x, int y, int[,] field)
+    private void TryEatRabbit(int x, int y, int[,] field)
     {
         for (var i = x-1; i <= x+1; i++)
         for (var j = y - 1; j <= y + 1; j++)
-            if (field[i, j] == 1)
+            if (i < 0 || i >= N || j < 0 || j >= M || i == x && j == y) continue;
+            else if (field[i, j] == 1)
             {
-                field[i, j] = 0;
-                return;
+                var locker = new object();
+                lock (locker)
+                {
+                    field[i, j] = 2;
+                    field[x, y] = 0;
+                    return;
+                }
             }
     }
 
-    public void PrintField()
+    public int[,] GetMap()
     {
-        var field = _field;
-        for (int i = 1; i < field.GetLength(0)-1; i++)
-        {
-            var str = String.Empty;
-            for (int j = 1; j < field.GetLength(1)-1; j++)
-            {
-                str += field[i, j].ToString() + " ";
-            }
-            Console.WriteLine(str);
-        }
+        if (_isEmpty)
+            GenerateMap();
+        return _field;
+    }
+
+    private void GenerateMap()
+    {
+        var rnd = new Random();
+        for (var i = 0; i < N; i++)
+        for (var j = 0; j < M; j++)
+            _field[i, j] = rnd.Next(3);
+        _isEmpty = false;
     }
 }
